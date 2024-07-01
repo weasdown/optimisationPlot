fdcca6107bdbea67d0ba28295895af0a5bce6242
William Easdown Babb <34220924+weasdown@users.noreply.github.com>
2024-07-01 14:16:09 +0100

    Add plane at range 500m

--- optim.py
+++ optim.py
@@ -16,31 +16,30 @@ g = 9.81
 t, v = np.meshgrid(theta_range, v_range)
 sin = np.sin(theta_range)
 cos = np.cos(theta_range)
-t_deg = np.rad2deg(t)
-r = 2 / g * v**2 * sin * cos
+# t_deg = np.rad2deg(t)
+ra = 2 / g * v**2 * sin * cos
 
 # plot graph
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
 # Plot the surface.
-surf = ax.plot_surface(t_deg, v, r, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+surf = ax.plot_surface(t, v, ra, cmap=cm.coolwarm)  #,
+                       # linewidth=0, antialiased=False)
+ra_contour = ax.plot(t, v, 500)
 
 # Customize axes limits
-ax.set_xlim(0, 90)      # theta
+# ax.set_xlim(0, 90)      # theta
 ax.set_ylim(0, 110)    # velocity
-ax.set_zlim(0, 1200)    # range
+z_max = 1200
+ax.set_zlim(0, z_max)    # range
 
-ax.zaxis.set_major_locator(LinearLocator(13))  # add tick marks to Z-axis
-
-# ax.contourf(t, v, r, offset=500, colors='k')
-# ax.contour3D(t_deg, v, r, 3, colors='black')  # , colors='k')  # cmap='viridis')
+ax.zaxis.set_major_locator(LinearLocator(int(z_max/100)+1))  # add tick marks to Z-axis
 
 # # Add a color bar which maps values to colors
 # fig.colorbar(surf, shrink=0.5, aspect=5)
 
-ax.set_xlabel('theta (°)')
+ax.set_xlabel('theta (rad)')
 ax.set_ylabel('v (m/s)')
 ax.set_zlabel('range (m)')
 ax.set_title('Cannon range')
